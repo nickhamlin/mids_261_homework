0a7c6ddda611d58b28be6cb6dfd23921435cd738
nickhamlin <nickhamlin@gmail.com>
2016-01-18 21:43:11 -0500

    Cleaning up HW1

--- HW1/mapper.py
+++ HW1/mapper.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
+
+#HW 1.4 - Mapper Function
 import sys
 import re
-WORD_RE = re.compile(r"[\w']+") #Compile regex to easily parse complete words
+WORD_RE = re.compile(r"[\w']+")
 filename = sys.argv[1]
-findwords = sys.argv[2].lower() 
+findwords = sys.argv[2].lower().split() 
 with open (filename, "r") as myfile:
     for num,line in enumerate(myfile.readlines()):
         fields=line.split('\t') #parse line into separate fields
         subject_and_body=" ".join(fields[-2:]).strip()#parse the subject and body fields from the line, and combine into one string
-        words=re.findall(WORD_RE,subject_and_body) #create list of words
+        words=re.findall(WORD_RE,subject_and_body)
         for word in words:
             flag=0
             if word in findwords:
-                #This flag indicates to the reducer that a given word should be considered
-                #by the reducer when calculating the conditional probabilities
-                flag=1 
-                
-            #This will send one row for every word instance to the reducer.
+                flag=1
             print fields[0]+'\t'+fields[1]+'\t'+word+'\t1\t'+str(flag)
--- HW1/reducer.py
+++ HW1/reducer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+
+#HW 1.4 - Reducer Function
 from __future__ import division
 import sys
 from math import log
-
-words={}
 emails={}
+words={}
 spam_email_count=0 #number of emails marked as spam
 spam_word_count=0 #number of total (not unique) words in spam emails
 ham_word_count=0 #number of total (not unique) words in ham emails
@@ -13,7 +14,7 @@ for chunk in sys.argv[1:]:
     with open (chunk, "r") as myfile:
         for i in myfile.readlines():
             
-            #parse the incoming line
+            #parse the line
             result=i.split("\t")
             email=result[0]
             spam=int(result[1])
@@ -66,11 +67,11 @@ for j,email in emails.iteritems():
             try:
                 p_spam+=log(words[word]['p_spam'])
             except ValueError:
-                continue #This means that words that do not appear in a class will use the class prior
+                pass #This means that words that do not appear in a class will use the class prior
             try:
                 p_ham+=log(words[word]['p_ham'])
             except ValueError:
-                continue          
+                pass
     if p_spam>p_ham:
         spam_pred=1
     else:
