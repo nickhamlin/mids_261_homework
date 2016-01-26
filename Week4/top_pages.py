61d4143a9c3b8b89d647e7d38d31a8a6dec925b1
nickhamlin <nickhamlin@gmail.com>
2016-01-26 00:08:46 -0500

    Adding HW2 Submission and Week 4 Async content

---
+++ Week4/top_pages.py
@@ -0,0 +1,44 @@
+"""Find Vroots with more than 400 visits.
+
+This program will take a CSV data file and output tab-seperated lines of
+
+    Vroot -> number of visits
+
+To run:
+
+    python top_pages.py anonymous-msweb.data
+
+To store output:
+
+    python top_pages.py anonymous-msweb.data > top_pages.out
+"""
+from mrjob.job import MRJob
+import csv
+
+def csv_readline(line):
+    """Given a sting CSV line, return a list of strings."""
+    for row in csv.reader([line]):
+        return row
+
+class TopPages(MRJob):
+
+    def mapper(self, line_no, line):
+        """Extracts the Vroot that was visited"""
+        cell = csv_readline(line)
+        if cell[0] == 'V':
+            yield cell[1],1### FILL IN
+                  # What  Key, Value  do we want to output?
+
+    def reducer(self, vroot, visit_counts):
+        """Sumarizes the visit counts by adding them together.  If total visits
+        is more than 400, yield the results"""
+        count=0
+        total = sum(i for i in visit_counts)### FILL IN
+                # How do we calculate the total visits from the visit_counts?
+        if total > 400:
+            count+=1
+            yield vroot, count### FILL IN
+                  # What  Key, Value  do we want to output?
+        
+if __name__ == '__main__':
+    TopPages.run()
